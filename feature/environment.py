4674bf8492b5dc2f12e97812e4985fc9b6b585c5
PabloSVilu <pablo.savilu@gmail.com>
2025-06-25 16:05:22 -0400

    Se crea la feature registro_usuario.feature y la carpeta steps con los .py correspondientes

--- feature/environment.py
+++ feature/environment.py
@@ -1,17 +1,30 @@
 from app import create_app
 from DBManager import db
-from database import models
+from database.models import Curso
 
 def before_all(context):
-    # Crear app y contexto para pruebas
     context.app = create_app()
     context.app_context = context.app.app_context()
     context.app_context.push()
 
-    # Crear todas las tablas en la base de datos
-    db.create_all()
-
 def after_all(context):
     db.session.remove()
-    db.drop_all()  # Elimina todas las tablas de la base de datos después de las pruebas
+    db.drop_all() 
     context.app_context.pop()
+
+def before_scenario(context, scenario):
+    db.session.remove() 
+    db.drop_all()
+    db.create_all()
+
+    dummy_curso = Curso.query.filter_by(nombre="Curso General Para Grupos").first()
+    if not dummy_curso:
+        dummy_curso = Curso(nombre="Curso General Para Grupos", activa=True)
+        db.session.add(dummy_curso)
+        db.session.commit()
+
+    print(f"Base de Datos reiniciada para el escenario: {scenario.name}\n")
+
+def after_scenario(context, scenario):
+    db.session.remove()
+    print(f"Escenario terminado: {scenario.name}\n")
